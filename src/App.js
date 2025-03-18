da5002119b9940be4e37b3f781abe55d4a4752b5
Sly <mintahsylvesterkojo@gamil.com>
2025-03-18 19:01:23 +0000

    update our menu-items to navigate with useNavigate HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
+import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
+  console.log(HomePage);
+
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
